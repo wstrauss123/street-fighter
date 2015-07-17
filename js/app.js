5307821c10e78f6a0ff6fcfe3bea9a2e1c4b3ec3
wstrauss123 <wstrauss123@gmail.com>
2015-07-16 19:14:24 -0700

    7/16/15a

---
+++ js/app.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-ready').hide();
+        $('.ryu-still').show();
+    })
+    .mousedown(function() {
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function() {
+                $(this).hide();
+                $(this).css('left', '520px');
+            })
+    })
+    .mouseup(function() {
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    })
+
+    function playHadouken() {
+        $('#hadouken-sound')[0].volume = 0.5;
+        $('#hadouken-sound')[0].load();
+        $('#hadouken-sound')[0].play();
+    }
+
+    $(document).keydown(function(e) {
+        //if (e.keyCode == 88) {
+        if (e.keyCode) {
+            $('.ryu-ready').hide();
+            $('.ryu-cool').show();
+        }   
+    }) .keyup(function(e) {
+        //if (e.keyCode == 88) {
+        if (e.keyCode) {
+            $('.ryu-cool').hide();
+            $('.ryu-ready').show();
+        }
+    })
+
+});
